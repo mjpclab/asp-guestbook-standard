a32727f1ad6cd4e96466e96450cd33e1269ff8a8
marjune <marjune@163.com>
2015-03-14 14:04:26 +0800

    reduce search range of table-select

--- js/table-select.js
+++ js/table-select.js
@@ -3,7 +3,7 @@
 		var name = this.name;
 		var checked = this.checked;
 
-		var $checkBoxes = $('input[name="' + name + '"][type=checkbox]').not(this);
+		var $checkBoxes = $('input[name="' + name + '"][type=checkbox]',this.form).not(this);
 		$checkBoxes.each(function (index, checkBox) {
 			checkBox.checked = checked;
 		});
