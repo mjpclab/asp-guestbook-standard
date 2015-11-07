dfed6a48d5101da9141ff397a9d87efe189e2eec
marjune <marjune@163.com>
2015-11-07 12:30:07 +0800

    improve scripts

--- js/refresh-captcha.js
+++ js/refresh-captcha.js
@@ -1,6 +1,6 @@
 (function enableRefreshCaptcha() {
 	var captcha = document.getElementById('captcha');
-	captcha.onclick = function() {
-		this.src = this.src.replace(/t=\d*/,'t=' + (new Date()).getTime());
+	captcha.onclick = function () {
+		this.src = this.src.replace(/t=\d*/, 't=' + (new Date()).getTime());
 	};
 }());
--- js/table-select.js
+++ js/table-select.js
@@ -1,14 +1,9 @@
-(function () {
-	function checkAllHandler() {
+(function enableCheckAll() {
+	$('table thead input[type=checkbox]').change(function () {
 		var name = this.name;
 		var checked = this.checked;
 
-		var $checkBoxes = $('input[name="' + name + '"][type=checkbox]',this.form).not(this);
-		$checkBoxes.each(function (index, checkBox) {
-			checkBox.checked = checked;
-		});
-	}
-
-	var $headerChecks = $('table thead input[type=checkbox]');
-	$headerChecks.change(checkAllHandler);
+		var $checkBoxes = $('input[name="' + name + '"][type=checkbox]', this.form).not(this);
+		$checkBoxes.prop('checked', checked);
+	});
 }());
