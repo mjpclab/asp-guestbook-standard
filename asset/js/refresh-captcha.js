68b836699c9512ac5c560987f9abda3f0e173c8b
marjune <marjune@163.com>
2016-02-18 23:58:13 +0800

    fix js issue when captcha is disabled

--- asset/js/refresh-captcha.js
+++ asset/js/refresh-captcha.js
@@ -1,6 +1,8 @@
 (function enableRefreshCaptcha() {
 	var captcha = document.getElementById('captcha');
-	captcha.onclick = function () {
-		this.src = this.src.replace(/t=\d*/, 't=' + (new Date()).getTime());
-	};
+	if(captcha) {
+		captcha.onclick = function () {
+			this.src = this.src.replace(/t=\d*/, 't=' + (new Date()).getTime());
+		};
+	}
 }());
